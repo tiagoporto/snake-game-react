c05c574fe3c8d8934f76275ef152240e5f1e0337
Tiago Porto <tiago@tiagoporto.com>
2025-09-24 19:58:00 -0300

    Set vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/snake-game-react/',
   plugins: [
     tailwindcss(),
     react({
